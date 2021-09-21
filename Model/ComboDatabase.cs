9729d9a6f26aeebce9343f36e7c3f2bbbc101860
UlisesRodriguezPerez <47537584+UlisesRodriguezPerez@users.noreply.github.com>
2021-09-20 23:06:18 -0600

    Prototipo del Json

---
+++ Model/ComboDatabase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Encodings;
+using System.Text.Json.Serialization;
+
+namespace Caso1.Model
+{
+    class ComboDatabase
+    {
+
+        public static void loadData() 
+        {
+            
+        }
+
+    }
+}
